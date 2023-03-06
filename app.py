71ae835726c1c4444a718a6e270c1bac033d1644
Nde Lucien <38033535+dark-matter08@users.noreply.github.com>
2023-03-06 08:45:15 +0100

    First get and oposts requests

--- app.py
+++ app.py
@@ -1,16 +1,95 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+book_list = [
+    {
+        "id": 0,
+        "author": "Chinua Achebe",
+        "language": "English",
+        "title": "Things Fall Apart"
+    },
+    {
+        "id": 1,
+        "author": "Hans Christian Anderson",
+        "language": "Danish",
+        "title": "Fairy Tales"
+    },
+    {
+        "id": 2,
+        "author": "Samuel Becket",
+        "language": "French, English",
+        "title": "Moly, Malone Dies, The Unnamable, the trilogy"
+    },
+    {
+        "id": 3,
+        "author": "Giovanni Boccaccio",
+        "language": "Italian",
+        "title": "The Decameron"
+    }
+]
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return '<h1>Hello Welome to my data stuff</h1>'
 
 
 @app.route('/<name>')
 def print_name(name):
     return 'Hi, Mr. {}'.format(name)
+
+
+@app.route('/books', methods=['GET', 'POST'])
+def books():
+    if request.method == 'GET':
+        if len(book_list) > 0:
+            return jsonify(book_list)
+
+        else:
+            'Nothing Found', 404
+
+    if request.method == 'POST':
+        print(request.form)
+        new_author = request.form['author']
+        new_language = request.form['language']
+        new_title = request.form['title']
+        new_id = book_list[-1]['id'] + 1
+
+        new_obj = {
+            'id': new_id,
+            'author': new_author,
+            'language': new_language,
+            'title': new_title
+        }
+
+        book_list.append(new_obj)
+        return jsonify(book_list), 201
+
+
+@app.route('/book/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def single_book(id):
+    if request.method == 'GET':
+        for book in book_list:
+            if book['id'] == id:
+                return jsonify(book)
+            pass
+
+    if request.method == 'PUT':
+        for book in book_list:
+            if book['id'] == id:
+                book['author'] = request.form['author']
+                book['language'] = request.form['language']
+                book['title'] = request.form['title']
+
+                updated_book = {
+                    'id': id,
+                    'author': book['author'],
+                    'language': book['language'],
+                    'title': book['title']
+                }
+
+                return jsonify(updated_book)
 
 
 if __name__ == '__main__':
